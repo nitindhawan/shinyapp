7afe0a9686ee6c22d0df25963636a31afafcbffc
Nitin Dhawan <ndhawan@zynga.com>
2014-07-28 02:25:47 +0530

    First Commit
    First Commit

---
+++ server.R
@@ -0,0 +1,17 @@
+library(shiny)
+library(ggplot2)
+
+shinyServer(function(input, output) {
+  
+  output$plot <- reactivePlot(function() {
+    
+    p <- qplot(mtcars$mpg, mtcars[, input$x], color=factor(mtcars[, input$color]),
+               xlab=input$x, ylab="mpg", geom=c("point", "smooth"))
+    
+    p <- p + labs(colour = input$color)
+    
+    print(p)
+    
+  }, height=400)
+    
+})
